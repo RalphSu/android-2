6ebabc05184a458ab5e2dfd4deccaac056af9c48
wuzhengyu <saky721@163.com>
2014-07-31 14:35:27 +0800

    add

--- StartM/src/test/Test2.java
+++ StartM/src/test/Test2.java
@@ -1,10 +1,26 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 public class Test2 {
 
 	public static void main(String[] args) {
 		System.out.println("=======================");
-
+		List<String> aList = new ArrayList<String>();
+		for (int i = 1; i < 10; i++)
+			aList.add(i + "");
+		
+		String start="9";
+		int s=aList.indexOf(start);
+		for (int i = s; i < aList.size(); i++) {
+			System.out.println(i+":"+aList.get(i));
+		}
+		
+		Iterator< String> iterator=aList.iterator();
+		ListIterator< String> listIterator=aList.listIterator(s);
 	}
 
 }
---
+++ StartX/src/com/aozhi/client/BrushService.java
@@ -0,0 +1,176 @@
+package com.aozhi.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.aozhi.client.util.PropertiesLoder;
+import com.aozhi.client.util.RegistryUtil;
+/**
+ * 
+ * @author wuyicun
+ *
+ */
+public class BrushService {
+	private Logger logger = LoggerFactory.getLogger(BrushService.class);
+	private PropertiesLoder propertiesLoder = null;
+
+	private String ADB = null;
+	private String STARTLAUNCHER = "";
+	private String STOPLAUNCHER = "";
+	private List<String> guidList = null;
+	private String[] RUNACTIVITYS = null;
+	private String[] FILES = null;
+	private final String separator = ",";
+
+	public void start(String[] args) {
+		logger.info("======> 加载启动参数");
+		loadProperties(args);
+		logger.info("======> 加载IMEI文件");
+		loadImeiFiles();
+		logger.info("======> 加载当前注册表的IMEI");
+		int startIndex=getStartImeiIndex();
+		for(int i=startIndex;i<guidList.size();i++) {
+			String guid=guidList.get(i);
+			logger.info("======>修改IMEI: " + guid);
+			setUserGuid(guid);
+			logger.info("======>启动ADB服务");
+			startAdbService();
+			logger.info("======>启动blueStacks");
+			startBlueStacks();
+			logger.info("======>打开应用");
+			startApp();
+			logger.info("======>退出blueStacks");
+			exitBlueStacks();
+		}
+	}
+
+	/**
+	 * 获取启动时的IMEI
+	 * @return
+	 */
+	private int getStartImeiIndex() {
+		int startIndex=0;
+		String current=RegistryUtil.getGUID();
+		if (guidList.contains(current)) {
+			int index=guidList.indexOf(current);
+			if (index+1!=guidList.size()) {
+				startIndex=index+1;
+			}
+		}
+		return startIndex;
+	}
+
+	/**
+	 * 启动ADB服务
+	 */
+	public void startAdbService() {
+		runCommand(buildAdbCommand("kill-server"));
+		delay(1);
+		runCommand(buildAdbCommand("start-server"));
+		delay(1);
+	}
+
+	/**
+	 * 启动模拟器
+	 */
+	public void startBlueStacks() {
+		runCommand(STARTLAUNCHER);
+		delay(30);
+	}
+
+	/**
+	 * 退出模拟器
+	 */
+	public void exitBlueStacks() {
+		runCommand(STOPLAUNCHER);
+		delay(10);
+	}
+
+	private void startApp() {
+		for (String activity : RUNACTIVITYS) {
+			runCommand(buildActivityRunCommand(activity));
+			delay(4);
+		}
+	}
+
+	private void loadProperties(String[] resourcesPaths) {
+
+		propertiesLoder = new PropertiesLoder(resourcesPaths);
+		ADB = propertiesLoder.getProperty("ADB.Path");
+		STARTLAUNCHER = propertiesLoder.getProperty("BlueStacks.Start");
+		STOPLAUNCHER = propertiesLoder.getProperty("BlueStacks.Exit");
+		RUNACTIVITYS = propertiesLoder.getProperty("Run.Activitys").split(separator);
+		FILES = propertiesLoder.getProperty("GUID.FILES").split(separator);
+	}
+
+	private void loadImeiFiles() {
+
+		guidList = RegistryUtil.read(FILES);
+	}
+
+	/**
+	 * 修改注册表GUID项
+	 * 
+	 * @param guid
+	 */
+	private void setUserGuid(String guid) {
+		RegistryUtil.setUSERGUID(guid);
+		RegistryUtil.setBootParameters(guid);
+	}
+
+	private void runCommand(String cmd) {
+		try {
+			logger.debug(" ------->执行命令:" + cmd);
+			StringBuffer buffer = new StringBuffer();
+			Process process = Runtime.getRuntime().exec(cmd);
+			InputStream fis = process.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+			String line = null;
+			String lineSeparator = System.getProperty("line.separator");
+			while ((line = br.readLine()) != null) {
+				buffer.append(line).append(lineSeparator);
+			}
+			logger.debug(buffer.toString());
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	private void delay(int seccond) {
+		try {
+			Thread.sleep(seccond * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 生成运行Activity脚本
+	 * 
+	 * @param activity
+	 * @return
+	 */
+	private String buildActivityRunCommand(String activity) {
+		// adb shell am start -n com.demo.app1/.activity.AppMain
+		StringBuffer buf = new StringBuffer();
+		buf.append(ADB);
+		buf.append(" shell am start -n ");
+		buf.append(activity);
+
+		return buf.toString();
+	}
+
+	private String buildAdbCommand(String cmd) {
+		StringBuffer buf = new StringBuffer();
+		buf.append(ADB);
+		buf.append(" ");
+		buf.append(cmd);
+
+		return buf.toString();
+	}
+}
